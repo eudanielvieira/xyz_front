4b2490c6b82b566e781b3b4d5a171c4fa4ed2b0a
Daniel Vieira <danielsrosa13@gmail.com>
2021-02-06 23:50:27 -0300

    feat: home added

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,3 +1,25 @@
-const Home = () => 'hello world deploy test';
+import React from 'react';
+import logo from 'assets/images/logo.svg';
+import styled from 'styled-components';
+
+const Home = () => {
+  const title = 'Daniel Vieira';
+  return (
+    <Body>
+      <Image src={logo} alt={`Logo ${title}`} />
+    </Body>
+  );
+};
+const Body = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #f2f2f2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Image = styled.img`
+  height: 200px;
+`;
 
 export default Home;
